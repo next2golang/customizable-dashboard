a4c16e39c839c17438757f3635fb158461596e91
david-t3 <hidavidk777@gmail.com>
2024-03-07 22:47:34 +0900

    feat: add rainbowkit for wallet connect.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,84 @@ import type { AppType, AppProps } from 'next/app';
 import type { Session } from "next-auth";
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from "next-auth/react";
-import '~/styles/globals.css';
+import { useRouter } from 'next/router';
+
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  Locale,
+  getDefaultConfig,
+  createAuthenticationAdapter,
+  RainbowKitAuthenticationProvider,
+} from '@rainbow-me/rainbowkit';
+
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import { WagmiProvider } from 'wagmi';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+  zora,
+} from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Layout } from '~/components/layout';
+
+import '~/styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+
+const { wallets } = getDefaultWallets();
+
+const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: 'YOUR_PROJECT_ID',
+  wallets: [
+    ...wallets,
+    {
+      groupName: 'Other',
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    zora,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) => {
+  const { locale } = useRouter() as { locale: Locale };
   return (
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     <SessionProvider session={session}>
-
-      <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider locale={locale}>
+            <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </ThemeProvider>
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </SessionProvider>
   );
 };
