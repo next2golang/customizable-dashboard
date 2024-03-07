eeb000a78df2396aee2ec51c5b4a7a85c81e887d
david-t3 <hidavidk777@gmail.com>
2024-03-07 14:34:45 +0900

    feat: update _app for sessionprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
-import { type AppType } from 'next/app';
+import { type AppType, AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from "next-auth/react";
 import '~/styles/globals.css';
 
 import { Layout } from '~/components/layout';
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => {
   return (
+    <SessionProvider session={session}>
       <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
+    </SessionProvider>
   );
 };
 
