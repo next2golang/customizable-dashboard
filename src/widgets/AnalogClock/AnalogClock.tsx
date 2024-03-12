79a1dc8260e71bca506ecb4ea016238d85de2d5c
david-t3 <hidavidk777@gmail.com>
2024-03-12 17:35:11 +0900

    feat: add AnalogClock widget skeleton.

---
+++ src/widgets/AnalogClock/AnalogClock.tsx
@@ -0,0 +1,28 @@
+import json from './AnalogClock.json';
+import Widget from '../../components/Widget/Widget';
+import { useEffect, useState } from 'react';
+// import _ from 'lodash';
+import Clock from './Clock';
+
+type Props = {
+  wid: string;
+};
+
+export default function AnalogClock({ wid }: Props) {
+  return (
+    <Widget
+      wid={wid}
+      schema={json.schema}
+      w={json.info.w}
+      h={json.info.h}
+      cn="overflow-hidden flex justify-center"
+      onSettings={({ }) => { }}
+      render={({ settings }) => {
+        return (
+          <div className="p-2 flex">
+          </div>
+        );
+      }}
+    />
+  );
+}
